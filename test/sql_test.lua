60225ef1e59a4053494a8fb8a68a344abdd2243f
sanikoyes <sanikoyes@163.com>
2019-08-02 16:03:27 +0800

    update sql test

--- test/sql_test.lua
+++ test/sql_test.lua
@@ -3,27 +3,36 @@ package.cpath = package.cpath .. ';../build/lib/?.so'
 local QtCore = require 'qtcore'
 local QtSql = require 'qtsql'
 
-local db = QtSql.QSqlDatabase.addDatabase("QSQLITE", "conn1")
-db:setDatabaseName("numbers.db")
+local db = QtSql.QSqlDatabase.addDatabase('QSQLITE', 'CONN1')
+db:setDatabaseName(':memory:')
 if not db:open() then
 	local err = db:lastError()
 	print('!!!', err:text():toLocal8Bit())
 end
+
 print('ok', db)
 
-db:exec("CREATE TABLE IF NOT EXISTS tab (n INT)")
+db:exec('CREATE TABLE IF NOT EXISTS tab (n INT)')
 
 local q = QtSql.QSqlQuery(db)
-q:prepare("INSERT INTO tab VALUES (:n)")
+q:prepare('INSERT INTO tab VALUES (:n)')
 
 for i=1,10 do
-	q:bindValue("n", QtCore.QVariant(i))
+	local v = QtCore.QVariant(i)
+	-- TODO:why string placeholder not works fine?
+	-- q:bindValue('n', v)
+	-- print(q:boundValue('n'):type())
+	q:bindValue(0, v)
+	print(q:boundValue(0):type())
 	q:exec()
 end
 
 local q2 = QtSql.QSqlQuery(db)
-q2:exec("SELECT * FROM tab")
+q2:exec('SELECT * FROM tab')
 while q2:next() do
-	local v = q2:value(0)
+	local v = q2:value('n')
 	print(v:toInt())
 end
+
+-- TODO:fix crash (comment this line)
+os.exit()
