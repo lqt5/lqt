0881acd9d76d2ed8836002444c26eafc7dfef922
sanikoyes <sanikoyes@163.com>
2019-08-23 16:12:05 +0800

    extract embed lua2h to embed.lua

--- generator/generator.lua
+++ generator/generator.lua
@@ -225,32 +225,10 @@ classes.process(idindex, typesystem, filterfiles)
 enums.output()
 classes.output()
 
--- generate ../common/lqt_addmethod.h
+-- generate embed .lua to .h files
 if module_name == 'qtcore' then
-	print '\tGenerate common/lqt_addmethod.h'
-	local fp = io.open(string.format('%s/../common/lqt_addmethod.lua', path), 'rb')
-	local script = fp:read '*all'
-	fp:close()
-
-	-- script = string.dump(loadstring(script, '@common/lqt_addmethod.lua'), false)
-
-	local bytes = {}
-	script:gsub('.', function(c)
-		if #bytes == 0 then
-			table.insert(bytes, '\t')
-		end
-		table.insert(bytes, string.format('%d, ', string.byte(c)))
-		-- table.insert(bytes, string.format('0x%02X, ', string.byte(c)))
-		if #bytes % 17 == 16 then
-			table.insert(bytes, '\n\t')
-		end
-	end)
-
-	fp = io.open(string.format('%s/../common/lqt_addmethod.h', path), 'wb')
-	fp:write(string.format('static const unsigned char add_method_string[] = {\n%s\n};\n'
-		, table.concat(bytes, '')
-	))
-	fp:close()
+	_G.path = path
+	require('embed')
 end
 
 --print_openmodule(module_name) -- does that
