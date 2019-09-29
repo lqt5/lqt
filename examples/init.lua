3c9560b6b757b43c0e6f6a1603d82eb0fc1f184f
sanikoyes <sanikoyes@163.com>
2019-09-29 15:19:39 +0800

    Remove all lqt registry data in instance or obj.__class etc.

--- examples/init.lua
+++ examples/init.lua
@@ -175,24 +175,39 @@ end)
 -- Cleanup lqt ref's before application shutdown
 --------------------------------------------------------------------------------
 local qCleanup = function()
-    -- cleanup registry ref class
+    -- Cleanup registry ref class
     local LQT_REF_CLASS = "Registry Ref Class"
     local registry = debug.getregistry()
     registry[LQT_REF_CLASS] = {}
-    -- remove lqt registry data in all instances
-    for _,inst in ipairs(QtCore.instances()) do
-        local env = debug.getfenv(inst)
+
+    -- Remove lqt registry data in all instances
+    local recycled = {}
+    local function recycle(inst, t)
+        -- Avoid cycle-reference, every t only recycle once
+        if recycled[t] then
+            return
+        end
+        recycled[t] = t
+
         local removals = {}
-        for k,v in pairs(env) do
+        for k,v in pairs(t) do
+            -- Cleanup lqt registry data
             if k:match('^[%*]?Lqt') then
                 table.insert(removals, k)
+            elseif type(v) == 'table' then
+                recycle(inst, v)
             end
         end
         for _,k in ipairs(removals) do
-            rawset(env, k, nil)
+            rawset(t, k, nil)
         end
     end
-    -- do full gc
+
+    for _,inst in ipairs(QtCore.instances()) do
+        local env = debug.getfenv(inst)
+        recycled(inst, env)
+    end
+    -- Do full gc
     gc()
 end
 --------------------------------------------------------------------------------
